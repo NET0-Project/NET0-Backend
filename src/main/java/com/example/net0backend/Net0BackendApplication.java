792b2e5425fbc92d4ce818061af03c589cae19ee
JunHyuk Yu <ericyoo0107@gmail.com>
2024-03-28 22:38:09 +0900

    first commit

---
+++ src/main/java/com/example/net0backend/Net0BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.net0backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Net0BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Net0BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/net0backend/Net0BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.net0backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Net0BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
