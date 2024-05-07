a5787ecbcd1c75d91e8c2d402090b8e0b235dd36
JunHyuk Yu <ericyoo0107@gmail.com>
2024-05-07 17:29:58 +0900

    feat: 내 현재 위치 정보를 받는 DTO 개발

---
+++ src/main/java/com/example/net0backend/dto/request/MyLocationRequest.java
@@ -0,0 +1,16 @@
+package com.example.net0backend.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+public class MyLocationRequest {
+    @NotNull(message = "내 위치의 위도 값은 필수값 입니다.")
+    @Range(min = 33, max = 43, message = "대한민국의 위도 값의 범위는 33 ~ 43 입니다.")
+    private Double x;
+
+    @NotNull(message = "내 위치의 경도 값은 필수값 입니다.")
+    @Range(min = 124, max = 132, message = "대한민국의 경도 값의 범위는 124 ~ 132 입니다.")
+    private Double y;
+}
