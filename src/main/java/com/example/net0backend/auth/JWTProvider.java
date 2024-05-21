28ab45ddad36ac2532d20c4236d7a873dd7f430b
JunHyuk Yu <ericyoo0107@gmail.com>
2024-05-21 17:20:22 +0900

    feat: JWT 토큰 검증 로직 구현

--- src/main/java/com/example/net0backend/auth/JWTProvider.java
+++ src/main/java/com/example/net0backend/auth/JWTProvider.java
@@ -1,7 +1,9 @@
 package com.example.net0backend.auth;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +11,19 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JWTProvider {
 
     @Value("${jwt.secret}")
     private String secretKey;
-    private final long EXPIRED_TIME = 2;
+    private TokenStatus tokenStatus;
+    private final long EXPIRED_TIME = 2; // 토큰 만료 시간 2H
+    public final String TOKEN_PRIFIX = "Bearer ";
+
+    /**
+     * JWT 토큰 생성
+     */
     public String createToken(JWTUserInfo userInfo) {
         Date expiredDate = Date.from(Instant.now().plus(EXPIRED_TIME, ChronoUnit.HOURS));
         return Jwts.builder()
@@ -31,7 +40,32 @@ public class JWTProvider {
         return null;
     }
 
-    public boolean validateToken(String token) {
-        return false;
+    /**
+     * 토큰 유효성 검사
+     */
+    public TokenStatus validateToken(String authorizationHeader) {
+        String token = getAccessToken(authorizationHeader);
+        try{
+            Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+            return TokenStatus.VALID;
+        } catch (ExpiredJwtException ex) {
+            log.info("token is Expired: {}", ex.getMessage());
+            return TokenStatus.EXPIRED;
+        }
+        catch (Exception ex) {
+           log.info("token Exception: {}", ex.getMessage());
+            return TokenStatus.INVALID;
+        }
+    }
+
+    /**
+     * 헤더에서 토큰 추출
+     */
+    private String getAccessToken(String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PRIFIX)) {
+            return authorizationHeader.substring(TOKEN_PRIFIX.length());
+        }
+        return authorizationHeader;
     }
 }
