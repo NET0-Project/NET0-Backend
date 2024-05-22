d6525273ef8c26b286a986ff5dc52c7e9300c47c
JunHyuk Yu <ericyoo0107@gmail.com>
2024-05-22 19:29:35 +0900

    feat: 토큰 검증 후 이메일 반환 로직 추가

--- src/main/java/com/example/net0backend/auth/JWTProvider.java
+++ src/main/java/com/example/net0backend/auth/JWTProvider.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -44,7 +45,7 @@ public class JWTProvider {
      * 토큰 유효성 검사
      */
     public TokenStatus validateToken(String authorizationHeader) {
-        String token = getAccessToken(authorizationHeader);
+        String token = parseBearerToken(authorizationHeader);
         try{
             Jwts.parser().setSigningKey(secretKey)
                     .parseClaimsJws(token);
@@ -52,18 +53,36 @@ public class JWTProvider {
         } catch (ExpiredJwtException ex) {
             log.info("token is Expired: {}", ex.getMessage());
             return TokenStatus.EXPIRED;
-        }
-        catch (Exception ex) {
-           log.info("token Exception: {}", ex.getMessage());
+        } catch (Exception ex) {
+            log.info("token Exception: {}", ex.getMessage());
             return TokenStatus.INVALID;
+        }
+    }
+
+    /**
+     * 토큰 유효성 검사 후 사용자 이메일 반환
+     */
+    public String validateTokenReturnEmail(String authorizationHeader) {
+        String token = parseBearerToken(authorizationHeader);
+        try {
+            return Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(token).getBody().getSubject();
+        } catch (ExpiredJwtException ex) {
+            log.info("token is Expired: {}", ex.getMessage());
+            // 토큰 삭제 로직 추가
+            throw new RuntimeException("Token is Expired");
+        } catch (Exception ex) {
+            log.info("token Exception: {}", ex.getMessage());
+            // 토큰 삭제 로직 추가
+            throw new RuntimeException("Token is Not Valid");
         }
     }
 
     /**
      * 헤더에서 토큰 추출
      */
-    private String getAccessToken(String authorizationHeader) {
-        if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PRIFIX)) {
+    private String parseBearerToken(String authorizationHeader) {
+        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith(TOKEN_PRIFIX)) {
             return authorizationHeader.substring(TOKEN_PRIFIX.length());
         }
         return authorizationHeader;
