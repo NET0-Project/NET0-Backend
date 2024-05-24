1781cef7d8df50ffda0ba9112ded94e7b37b033c
JunHyuk Yu <ericyoo0107@gmail.com>
2024-05-24 18:46:30 +0900

    bug: between 은 작은것 ~ 큰 것 순서로 적어야 함

--- src/main/java/com/example/net0backend/repository/ShopRepository.java
+++ src/main/java/com/example/net0backend/repository/ShopRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
 
-    @Query("SELECT shop from Shop shop where (shop.latitude BETWEEN :x+1.0 and :x-1.0) and (shop.longitude BETWEEN :y+1.0 and :y-1.0)")
+    @Query("SELECT shop from Shop shop where (shop.latitude BETWEEN :x-1.0 and :x+1.0) and (shop.longitude BETWEEN :y-1.0 and :y+1.0)")
     List<Shop> findNearShopList(@Param("x") Double x, @Param("y") Double y);
 }
