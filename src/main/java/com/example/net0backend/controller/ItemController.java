0511b4d1294af2b0df9ac3c091f80ead2164527d
tibetyo <zjhj0814@gamil.com>
2024-05-28 23:58:57 +0900

    refactor : 가게 상품 정보 조회 API 스펙 수정

--- src/main/java/com/example/net0backend/controller/ItemController.java
+++ src/main/java/com/example/net0backend/controller/ItemController.java
@@ -5,12 +5,16 @@ import com.example.net0backend.dto.ItemResponse;
 import com.example.net0backend.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,9 +25,12 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("/{storeId}")
-    public List<ItemResponse> showItemSearchByStore(@PathVariable Long storeId){
+    public ResponseEntity<Map<String,Object>> showItemSearchByStore(@PathVariable Long storeId){
         //findStoreById 개발 이후 throw StoreNotFoundException 추가하기
         log.info("storeId = "+storeId);
-        return itemService.getItemsByStore(storeId);
+        Map<String, Object> result = new HashMap<>();
+        result.put("storeId",storeId);
+        result.put("items",itemService.getItemsByStore(storeId));
+        return ResponseEntity.ok().body(result);
     }
 }
