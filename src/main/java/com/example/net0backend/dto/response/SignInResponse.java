1ab5bf4c5b6f605479f260b1c7d32563e8ce6a22
JunHyuk Yu <ericyoo0107@gmail.com>
2024-05-30 17:03:00 +0900

    feat: 로그인 시 토큰 생성 시간도 함께 전송

--- src/main/java/com/example/net0backend/dto/response/SignInResponse.java
+++ src/main/java/com/example/net0backend/dto/response/SignInResponse.java
@@ -3,16 +3,20 @@ package com.example.net0backend.dto.response;
 import lombok.Data;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
+
 @Data
 public class SignInResponse {
     private String accessToken;
     private String refreshToken;
+    private LocalDateTime tokenCreatedAt;
     private int expirationTime;
     private String tokenType;
 
     private SignInResponse(String accessToken, String refreshToken) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.tokenCreatedAt = LocalDateTime.now();
         this.expirationTime = 3600;
         this.tokenType = "Bearer";
     }
